8c087e56ef08f1575756fa3bfc7b392a0ca5b5d8
Renan <renan.vamo@gmail.com>
2021-06-09 20:10:20 -0300

    Primeiro commit do projeto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      My Favourite Movie List
+      </header>
+    </div>
+  );
+}
+
+export default App;
