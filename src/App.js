6ba3f912bc9b6f83546ed3c1b8dd937bd84b18bf
Renan <renan.vamo@gmail.com>
2021-06-10 19:22:34 -0300

    Adicionei alguns componentes, o fetch e o state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React from 'react';
+import MovieListHeading from './Components/MovieListHeading';
+import SearchBox from './Components/SearchBox'
+import MovieCards from './Components/MovieCards';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-      My Favourite Movie List
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.handleSearch = this.handleSearch.bind(this);
+    this.state = {
+      movies: [],
+      searchValue: '',
+    };
+  }
+
+  handleSearch({ target }) {
+    this.setState({
+      searchValue: target.value
+    });
+    this.getMovies()
+  }
+
+  async getMovies() {
+    const { searchValue } = this.state;
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=a05e989d`;
+    await fetch(url)
+      .then(response => response.json()
+      .then((movies) => this.setState({ movies: movies.Search })));
+  }
+  
+  render() {
+    const { movies, searchValue } = this.state;
+    return (
+      <div className='container-fluid movie-app'>
+        <div className='row d-flex align-items-center mt-4 mb-4'>
+          <MovieListHeading heading='My Favourite Movies' />
+          <SearchBox searchValue={ searchValue } handleSearch={ this.handleSearch }/>
+        </div>
+        <div className='row'>
+          <MovieCards movies={ movies } />
+        </div>
+		  </div>
+    );
+  }
 }
 
 export default App;
